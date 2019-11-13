442487780b3d3e2f8b94fdbe3bf8b723e23a55b0
robyafrizal <robyafrizal86@gmail.com>
2019-11-13 16:31:57 +0700

    Add Jumlah Follower

--- index.js
+++ index.js
@@ -3,9 +3,21 @@ function getFollowers() {
   fetch("https://api.github.com/users/robyafrizal/followers")
     .then(response => response.json())
     .then(follower => {
+      let jumlahFollower = document.createElement("h3");
+      jumlahFollower.innerText = `Jumlah Follower : ${follower.length}`;
+      jumlahFollower.className = "text-center";
+      document.getElementById("div").append(jumlahFollower);
+      //   console.log(follower.length);
+
       follower.map(data => {
         let image = document.createElement("figure");
         image.className = "figure m-4";
+
+        let link = document.createElement("a");
+        link.className = "text-center";
+        link.style = "font-size:10px";
+        link.href = data.html_url;
+        // link.innerText = data.html_url;
 
         let img = document.createElement("img");
         img.setAttribute("src", data.avatar_url);
@@ -13,11 +25,15 @@ function getFollowers() {
         img.style = "height: 200px";
 
         let text = document.createElement("figcaption");
-        text.className = "figure-caption text-center";
+        text.className = "figure-caption text-center m-3";
+        text.href = data.html_url;
         text.innerText = data.login;
 
-        image.append(img);
-        image.append(text);
+        link.append(img);
+        link.append(text);
+        // image.append(img);
+        // image.append(text);
+        image.append(link);
         document.getElementById("div").append(image);
       });
     });
