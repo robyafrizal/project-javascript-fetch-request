cf4bc2df92c79b81b7d83dd20b5fc23dbfe5ddec
robyafrizal <robyafrizal86@gmail.com>
2019-11-13 14:54:03 +0700

    Add new data

---
+++ index.js
@@ -0,0 +1,27 @@
+//Menggunakan Fetch
+function getFollowers() {
+  fetch("https://api.github.com/users/robyafrizal/followers")
+    .then(response => response.json())
+    .then(follower => {
+      follower.map(data => {
+        let image = document.createElement("figure");
+        image.className = "figure m-4";
+
+        let img = document.createElement("img");
+        img.setAttribute("src", data.avatar_url);
+        img.className = "figure-img img-thumbnail rounded-circle";
+        img.style = "height: 200px";
+
+        let text = document.createElement("figcaption");
+        text.className = "figure-caption text-center";
+        text.innerText = data.login;
+
+        image.append(img);
+        image.append(text);
+        document.getElementById("div").append(image);
+      });
+    });
+}
+
+//Menggunakan Ajax
+// const req = new HMLHttpRequest
